2387813adf3c410edc42978d1efa26ac19603808
Horrid Entity <etienne.olieu@gmail.com>
2022-11-30 17:11:30 +0100

    url converter

---
+++ js/URLconverter.js
@@ -0,0 +1,25 @@
+class URLconverter{
+
+constructor(){
+
+}
+
+convertURL(){
+    let urlToBeConverted = document.getElementById('url-input').value;
+    //Get first half of url
+    const urlToArray = urlToBeConverted.split("file");
+
+    //Isolate the id og the img
+    let getIdOfImg = urlToArray[1].split("/");
+    urlToArray[1] = 'uc?export=view&id=' + getIdOfImg[2];
+
+    //Make one array - merge!
+    let converted = [urlToArray[0], urlToArray[1]].join('');
+    console.log(converted);
+}
+
+
+
+}
+
+var URLconvertClass = new URLconverter();
