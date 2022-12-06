bf273919094da741806ec4b4cb96eb7cf43387e8
annesofiefogh <anne2z08@stud.kea.dk>
2022-12-06 14:46:27 +0100

    #first commit

--- js/backstageImages.js
+++ js/backstageImages.js
@@ -1,34 +1,49 @@
-class Backstage{
 
-    constructor(){
-    }
-
-    async getAllImages(){
+    let selectedUrl = null;
+    let data = [];
+    
+    async function getAllImages(){
         let response = await fetch('http://localhost:8080/get/allBackstageImages');
-        let data = await response.json();
+        data = await response.json();
 
         const gallery = document.getElementById('backstageGrid');
         $(gallery).empty();
-
-        let count = 0;
         
-        data.forEach(element => {
-        
-
-            //Creating the grid
+        for (let i in data) {
             let backstageGrid = document.createElement('div');
-            backstageGrid.setAttribute('class', 'col-md-4 bg-dark gallery');
+            backstageGrid.setAttribute('class', 'col-md-4 gallery');
 
-            let url = `${element.imageURL}`;
-            backstageGrid.innerHTML = `<img class="img-thumbnail grid" src=${url} onclick="backstage.showImage('${url}')" data-bs-toggle="modal" data-bs-target="#popupImage" data-slide-to="${count++}">`
+            let url = `${data[i].imageURL}`;
+            backstageGrid.innerHTML = `<img class="img-thumbnail grid" src=${url} onclick="showImage(${url})" data-bs-toggle="modal" data-bs-target="#popupImage"><br><br>`;
             gallery.appendChild(backstageGrid);
-        });
+        }
     }
 
-    showImage(url) {
+    function showImage(url) {
         let popup = document.getElementById('modalBody');
-        popup.innerHTML = `<br><br><br><br></br><img class="galleryPopup" src="${url}" alt="oops">`;
+        popup.innerHTML = `<br><br><br><br><br><img class="galleryPopup" src="${url}" alt="oops">`;
+        selectedUrl = url;
     }
 
-}
-let backstage = new Backstage();
+    function searchUrlInData(url){
+        for (let i = 0; i < data.length; i++){
+            if (data[i].imageURL.match(url)){
+                return i;
+            }
+            return 0;
+        }
+    }
+
+    function incrementCount(){
+        let i = searchUrlInData(selectedUrl);
+        i++;
+        if (i > data.length){
+            i = 0;
+            showImage(data[i].imageURL);
+        }
+    }
+
+    function decrementCount(){
+
+    }
+
