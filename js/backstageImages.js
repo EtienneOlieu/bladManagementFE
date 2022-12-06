1993e2c3235989eeec72fe2b64e38f03a3e11109
knud21kea <grah0070@stud.kea.dk>
2022-12-06 23:21:49 +0100

    modal switch

--- js/backstageImages.js
+++ js/backstageImages.js
@@ -1,6 +1,6 @@
 
     let selectedUrl = null;
-    let data = [];
+    let data = null;
     
     async function getAllImages(){
         let response = await fetch('http://localhost:8080/get/allBackstageImages');
@@ -14,36 +14,49 @@
             backstageGrid.setAttribute('class', 'col-md-4 gallery');
 
             let url = `${data[i].imageURL}`;
-            backstageGrid.innerHTML = `<img class="img-thumbnail grid" src=${url} onclick="showImage(${url})" data-bs-toggle="modal" data-bs-target="#popupImage"><br><br>`;
+            backstageGrid.innerHTML = `<img class="img-thumbnail grid" src=${url} onclick="showImage('${url}')"><br><br>`;
             gallery.appendChild(backstageGrid);
         }
     }
 
     function showImage(url) {
         let popup = document.getElementById('modalBody');
-        popup.innerHTML = `<br><br><br><br><br><img class="galleryPopup" src="${url}" alt="oops">`;
+        popup.innerHTML = `<img class="galleryPopup" src="${url}" alt="oops">`;
         selectedUrl = url;
     }
 
-    function searchUrlInData(url){
+    function searchUrlInData(){
+        let match = -1;
         for (let i = 0; i < data.length; i++){
-            if (data[i].imageURL.match(url)){
-                return i;
-            }
-            return 0;
+            if (data[i].imageURL === selectedUrl){
+                match = i;
+            }            
         }
+        return match;
     }
 
     function incrementCount(){
-        let i = searchUrlInData(selectedUrl);
+        let i = searchUrlInData();
         i++;
-        if (i > data.length){
+        if (i > data.length-1){
             i = 0;
-            showImage(data[i].imageURL);
         }
+            let popup = document.getElementById('modalBody');
+            let url = `${data[i].imageURL}`;
+        popup.innerHTML = `<img class="galleryPopup" src="${url}" alt="oops">`;
+        selectedUrl = url;
+        
     }
 
     function decrementCount(){
-
+        let i = searchUrlInData();
+        i--;
+        if (i < 0){
+            i = data.length-1;
+        }
+            let popup = document.getElementById('modalBody');
+            let url = `${data[i].imageURL}`;
+        popup.innerHTML = `<img class="galleryPopup" src="${url}" alt="oops">`;
+        selectedUrl = url;
     }
 
