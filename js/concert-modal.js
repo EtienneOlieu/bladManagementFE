681ee50f24c385d36c8548669ee22d92fdae2e4e
knud21kea <grah0070@stud.kea.dk>
2022-12-12 13:47:27 +0100

    #unsubscribe

---
+++ js/concert-modal.js
@@ -0,0 +1,26 @@
+class ConcertModal {
+
+    constructor() {
+    }
+    
+    showModal() { 
+
+        alert("here")
+
+        /* let count = {}
+
+        this.data.forEach(function(item) {
+            var colour = item.colour
+            count[colour] = (count[colour] || 0) +1
+        });
+
+        for (let index in count) {           
+            let tableRow = `<tr style="vertical-align:middle;">
+            <td>${index}</td>
+            <td>${count[index]}</td>
+            </tr>;`
+            $('#pin-data').append(tableRow);
+        }    */      
+    }  
+} 
+var concertModal = new ConcertModal();
